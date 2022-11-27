23befc8f41c0df536a4ac6addd15314b30b53295
DreamBold <52863986+dreambold@users.noreply.github.com>
2022-11-27 21:01:01 +0000

    minor changes

--- config.js
+++ config.js
@@ -17,10 +17,10 @@
 export const config = {};
 
 // The OAuth client ID from the Google Developers console.
-config.oAuthClientID = '904179007379-pmp3kqovqrrk3trna0n59qtdhko7pg61.apps.googleusercontent.com';
+config.oAuthClientID = '-.apps.googleusercontent.com';
 
 // The OAuth client secret from the Google Developers console.
-config.oAuthclientSecret = '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';
+config.oAuthclientSecret = 'GOCSPX-';
 
 // The callback to use for OAuth requests. This is the URL where the app is
 // running. For testing and running it locally, use 127.0.0.1.
